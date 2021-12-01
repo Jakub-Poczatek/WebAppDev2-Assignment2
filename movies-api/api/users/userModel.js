c88389c147fd55d54c0a3a804e4e6568267e1b44
Jakub-Poczatek <kubapoczatek2001@gmail.com>
2021-12-01 23:59:42 +0000

    Mongo lab Exercises

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -2,11 +2,6 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-/*const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
-});*/
-
 const MovieSchema = new Schema({
     id: Number,
     title: String
