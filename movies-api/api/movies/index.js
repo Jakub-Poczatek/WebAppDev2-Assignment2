78dc719cfdb4cbf0d4b1532e81ec0858c9aca11c
Jakub-Poczatek <kubapoczatek2001@gmail.com>
2021-12-10 18:13:53 +0000

    Topic 11 - I've completed all my labs!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { movieReviews } from './moviesData';
+import {
+    getUpcomingMovies
+  } from '../tmdb-api';
 import uniqid from "uniqid";
 import movieModel from "./movieModel";
 import asyncHandler from "express-async-handler";
@@ -63,5 +66,10 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 export default router;
